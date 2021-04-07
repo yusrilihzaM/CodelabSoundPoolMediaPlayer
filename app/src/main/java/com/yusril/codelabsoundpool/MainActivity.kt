6fee7f690dd0ee6d275641d3155ef68529a09d7c
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-04-08 00:49:41 +0700

    MediaPlayer

--- app/src/main/java/com/yusril/codelabsoundpool/MainActivity.kt
+++ app/src/main/java/com/yusril/codelabsoundpool/MainActivity.kt
@@ -1,38 +1,66 @@
 package com.yusril.codelabsoundpool
 
+import android.annotation.SuppressLint
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var sp: SoundPool
-    private var soundId: Int = 0
-    private var spLoaded = false
+    private var mMediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnSound = findViewById<Button>(R.id.btn_soundpool)
-        sp = SoundPool.Builder()
-                .setMaxStreams(10)
-                .build()
-
-        sp.setOnLoadCompleteListener { _, _, status ->
-            if (status == 0) {
-                spLoaded = true
+        val btnPlay = findViewById<Button>(R.id.btn_play)
+        val btnStop = findViewById<Button>(R.id.btn_stop)
+        btnPlay.setOnClickListener {
+            if (!isReady) {
+                mMediaPlayer?.prepareAsync()
             } else {
-                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
+                if (mMediaPlayer?.isPlaying as Boolean) {
+                    mMediaPlayer?.pause()
+                } else {
+                    mMediaPlayer?.start()
+                }
             }
         }
-        soundId = sp.load(this, R.raw.yoasobi, 1) // in 2nd param u have to pass your desire ringtone
-
-        btnSound.setOnClickListener {
-            if (spLoaded) {
-                Toast.makeText(this@MainActivity, "Berhasil load", Toast.LENGTH_SHORT).show()
-                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+        btnStop.setOnClickListener {
+            if (mMediaPlayer?.isPlaying as Boolean || isReady) {
+                mMediaPlayer?.stop()
+                isReady = false
             }
         }
+        init()
     }
-
+    @SuppressLint("ObsoleteSdkInt")
+    private fun init() {
+        mMediaPlayer = MediaPlayer()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val attribute = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+            mMediaPlayer?.setAudioAttributes(attribute)
+        } else {
+            mMediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        }
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.yoasobi)
+        try {
+            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        mMediaPlayer?.setOnPreparedListener {
+            isReady = true
+            mMediaPlayer?.start()
+        }
+        mMediaPlayer?.setOnErrorListener { mp, what, extra -> false }
+    }
 }
